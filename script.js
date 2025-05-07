46fbbb70de877676559045afb9f7931687e25f34
Nitish kumar <109736430+NitishKumar345@users.noreply.github.com>
2025-05-07 15:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function() {
+  const target = document.getElementById("level");
+  let count = 0;
+  let current = target;
+
+  while (current) {
+    count++;
+    current = current.parentElement;
+  }
+
+  alert("The level of the element is: " + count);
+};
